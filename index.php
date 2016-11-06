6332859c3d3107a2ef9e78a67c41fa30aec62362
Chooper <chooper100.scratch@gmail.com>
2016-11-06 13:04:51 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- Page title -->
+        <title>FTP Connect</title>
+    </head>
+    <body>
+        <div class="start">
+            <h1>FTP Connect</h1>
+            <h2>The simple and free FTP client to use!</h2>
+            <h3>Get started:</h3>
+            <form method="post">
+                <div class="input-group">
+                    <label for="txtftphost">FTP Host:</label>
+                    <input type="text" id="txtftphost" />
+                </div>
+                <div class="input-group">
+                    <label for="txtuser">FTP Username:</label>
+                    <input type="text" id="txtuser" />
+                </div>
+                <div class="input-group">
+                    <label for="txtpass">FTP Password:</label>
+                    <input type="password" id="txtpass" />
+                </div>
+                <input type="submit" value="Go"/>
+            </form>
+        </div>
+    </body>
+</html>	
