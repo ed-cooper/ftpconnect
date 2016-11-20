07bad2fb0f7d7c1d15cd1b88bc979f45a373b876
chooper100 <chooper100.scratch@gmail.com>
2016-11-20 16:18:33 +0000

    Update legal page css

--- legal/index.php
+++ legal/index.php
@@ -15,37 +15,41 @@
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         
         <!-- Styles -->
-        <link href='/assets/css/home.css' rel='stylesheet' type='text/css'>
+        <link href='/assets/css/legal.css' rel='stylesheet' type='text/css'>
         
         <!-- Fonts -->
-        <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Roboto|Roboto+Slab' rel='stylesheet' type='text/css'>
         
         <!--Scripts-->
         <script src="/assets/js/home.js"></script>
     </head>
     <body>
-        <h1>Legal</h1>
-        <p>
-            I'm working hard to ensure that FTP Connect is as stable as possible.
-            However, during these early stages, I can't guarantee that any of your
-            files won't be lost. Make sure you take a backup of any files before using
-            this software - you might regret it if you don't!
-        </p>
-        <h2>
-            Disclaimer
-        </h2>
-        <p>
-            THIS SOFTWARE IS PROVIDED BY THE AUTHOR “AS IS” AND ANY EXPRESS OR IMPLIED
-            WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-            MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
-            EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-            SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
-            TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-            PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-            LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-            NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-            SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-        </p>
+        <div id="header">
+            <h1>Legal</h1>
+        </div>
+        <div id="main">
+            <p>
+                I'm working hard to ensure that FTP Connect is as stable as possible.
+                However, during these early stages, I can't guarantee that any of your
+                files won't be lost. Make sure you take a backup of any files before using
+                this software - you might regret it if you don't!
+            </p>
+            <h2>
+                Disclaimer
+            </h2>
+            <p>
+                THIS SOFTWARE IS PROVIDED BY THE AUTHOR “AS IS” AND ANY EXPRESS OR IMPLIED
+                WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+                MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+                EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+                SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+                TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+                PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+                LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+                NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+                SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+            </p>
+        </div>
         <?php include $_SERVER['DOCUMENT_ROOT'].'/parts/footer.php'; ?>
     </body>
 </html>
