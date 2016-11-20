f402243ae1d7f3a977d3ceeba8c21b3afa2b4b42
chooper100 <chooper100.scratch@gmail.com>
2016-11-20 17:23:19 +0000

    Update footer design

--- parts/footer.php
+++ parts/footer.php
@@ -1,3 +1,46 @@
 <div id="footer">
-    <a href="/legal">Legal</a>
+    <ul id="footer-nav">
+        <li>
+            <a href="/">Home</a>
+        </li>
+        <li>
+            <a href="/legal">Legal</a>
+        </li>
+    </ul>
+    <span id="copyright">&copy Chooper100, 2016</span>
 </div>
+<style>
+    #footer {
+        background-color: #e0e0e0;
+        overflow: hidden;
+        padding: 5px 10px;
+    }
+    
+    #footer-nav {
+        margin: 0;
+        padding: 0;
+        list-style-type: none;
+    }
+    
+    #footer-nav li {
+        float: left;
+        width: 80px;
+    }
+    
+    #footer-nav li a {
+        text-decoration: none;
+        display: block;
+        color: #666;
+        font-size: 11pt;
+    }
+    
+    #footer-nav li a:hover {
+        text-decoration: underline;
+    }
+    
+    #copyright {
+        float: right;
+        color: #666;
+        font-size: 11pt;
+    }
+</style>
