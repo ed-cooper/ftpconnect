f51471616223954070ad9608b01d154b9cfdce5e
chooper100 <chooper100.scratch@gmail.com>
2016-11-22 20:55:53 +0000

    Add 403 page

---
+++ content/403-forbidden.php
@@ -0,0 +1,17 @@
+<h2>
+    The server has refused to fulfill your request.
+</h2>
+<p>
+    <b>What you can do:</b>
+</p>
+<ul>
+    <li>Run and scream for help</li>
+    <li>Hide behind the sofa</li>
+</ul>
+<p>
+    <b>Alternatively, you can:</b>
+</p>
+<ul>
+    <li><a href="javascript:location.reload()">Reload the page</a> and try again</li>
+    <li><a href="https://scratch.mit.edu/users/chooper100">Contact the site admin</a></li>
+</ul>
