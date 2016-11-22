83f487fd136cd5c1d9bd50156fddd657d333c0fe
chooper100 <chooper100.scratch@gmail.com>
2016-11-22 20:34:01 +0000

    Add 404 page

---
+++ content/404-file-not-found.php
@@ -0,0 +1,20 @@
+<h2>
+    Sorry, but it looks like this page has been eaten by an evil kumquat.
+</h2>
+<p>
+    <b>What you can do:</b>
+</p>
+<ul>
+    <li>Run and scream for help</li>
+    <li>Hide behind the sofa</li>
+    <li>Activate the kumquat swatter</li>
+</ul>
+<p>
+    <b>Alternatively, you can:</b>
+</p>
+<ul>
+    <li>Check you didn't mis-type</li>
+    <li>Check your URL is all lowercase - URLs are CaSe SeNsItIve</li>
+    <li>Go to our <a href="/">home page</a></li>
+    <li><a href="https://scratch.mit.edu/users/chooper100">Contact the site admin</a></li>
+</ul>
