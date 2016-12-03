10c8ace965edc782b49f4e2b37827060dda6aa89
chooper100 <chooper100.scratch@gmail.com>
2016-12-03 21:51:51 +0000

    Make login form use xhr

--- parts/loginform.php
+++ parts/loginform.php
@@ -23,3 +23,29 @@ $allow_ssl = function_exists("ftp_ssl_connect");
     </div>
     <input type="submit" class="btn-submit" value="Go"/>
 </form>
+<script>
+var loginform = document.getElementById("loginform");
+
+loginform.onsubmit = function(e) {
+    var request = new XMLHttpRequest();
+        
+    request.onload = function() {
+        var response = JSON.parse(request.responseText);
+        if (response.success) {
+            alert("Logged in successfully!");
+        } else {
+            alert("Error logging in: " + response.message);
+        }
+    };
+
+    request.onerror = function() {
+        alert("Error sending web request. Please try again later.");
+    };
+
+    request.open("POST", "/api/login.php", true);
+    request.send(new FormData(loginform));
+    
+    e.preventDefault = true;
+    return false;
+};
+</script>
